434042a4cf898af946457be6355b6c95b6902516
Halouani <Halouani142@gmail.com>
2023-03-06 01:21:49 +0100

    "implemented Stripe payement"

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,48 +2,115 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
 use App\Repository\ShoppingCartItemRepository;
 use App\Repository\UserRepository;
+use DateTime;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class StripeController extends AbstractController
 {
-    #[Route('/stripe', name: 'app_stripe')]
+    #[Route('/commande/createSession', name: 'app_stripe')]
     public function index(ShoppingCartItemRepository $shoppingCartRepository): Response
     {
         $user = $this->getUser();
         if (!$user) {
             return new Response('Utilisateur introuvable', Response::HTTP_BAD_REQUEST);
         }
+        \Stripe\Stripe::setApiKey('sk_test_51MhGaAGeGEgrQ6hOFaUPvKPr8iOv7UjDwPJ22UAHMhCVD0VCQw3CmEGh0mQoVN7b635WeO2rilB94j2hSWMNDxhu00UQXAHDAc');
         $userItems = $shoppingCartRepository->findBy(['user' => $user]);
+        $stripeProducts = [];
         foreach ($userItems as $item) {
             $product = $item->getProduct();
+            $product = \Stripe\Product::create([
+                'name' => $product->getName(),
+                'images' => ["http://localhost:8000/uploads/products/" . $product->getImage()],
+            ]);
+
+            // Create a price for the product
+            $price = \Stripe\Price::create([
+                'unit_amount' => $item->getProduct()->getPrice() * 100, // Convert price to cents
+                'currency' => 'eur',
+                'product' => $product->id,
+            ]);
+
+            // Add the price to the Stripe products array
             $stripeProducts[] = [
-                'price_data' =>
-                [
-                    'currency' => 'eur',
-                    'unit_amount' => $product->getPrice(),
-                    'product_data' =>
-                    [
-                        'name' => $product->getName(),
-                        'images' => "http://localhost:8000/uploads/products/" . $product->getImage()
-                    ]
-                ],
-                'quantity' => $item->getQuantity()
+                'price' => $price->id,
+                'quantity' => $item->getQuantity(),
             ];
         }
-        \Stripe\Stripe::setApiKey('sk_test_51MhGaAGeGEgrQ6hOFaUPvKPr8iOv7UjDwPJ22UAHMhCVD0VCQw3CmEGh0mQoVN7b635WeO2rilB94j2hSWMNDxhu00UQXAHDAc');
+
         header('Content-Type: application/json');
         $YOUR_DOMAIN = 'http://localhost:8000';
         $checkout_session = \Stripe\Checkout\Session::create([
             'line_items' => [$stripeProducts],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'success_url' => $this->generateUrl('success_route', [], UrlGeneratorInterface::ABSOLUTE_URL),
+
+
             'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
         ]);
         return new JsonResponse(['id' => $checkout_session->id]);
+    }
+    #[Route('/success', name: 'success_route')]
+    public function success(UserRepository $userRepository, ManagerRegistry $doctrine): Response
+    {
+        $user2 = $this->getUser();
+        //check if the user is authenticated or not
+        if (!$user2) {
+            return new Response('Utilisateur introuvable', Response::HTTP_BAD_REQUEST);
+        }
+        $user = $userRepository->find($user2);
+        //get the user from the repository to get the necessary informations such as his address
+        $userAddress = $user->getAdresse();
+        $userItems = $user->getShoppingCartItems();
+        $commande = new Commande();
+        $currenttime = new DateTime();
+        $commande->setCreatedAt($currenttime);
+        $commande->setAddresse($userAddress);
+        // add all the shopping cart items to the commande
+        foreach ($userItems as $item) {
+            $commande->addItem($item);
+        }
+        $commande->setUser($user);
+        $commande->setIsConfirmed(0);
+        //check if all the product's quantities are enough
+        foreach ($userItems as $item) {
+            $product = $item->getProduct();
+            //get the available quantity of the product
+            $productAvailableQuantity = $product->getQuantity();
+            //get the quantity that will be substracted when confirming the order
+            $quantityTosubstract = $item->getQuantity();
+            $isAvailable = ($productAvailableQuantity - $quantityTosubstract) >= 0;
+            if (!$isAvailable) {
+                return new Response("Produit non disponible", Response::HTTP_BAD_REQUEST);
+            }
+        }
+        $stripeProducts = [];
+        foreach ($userItems as $item) {
+            $product = $item->getProduct();
+            //get the available quantity of the product
+            $productInitialQuantity = $product->getQuantity();
+            //get the quantity that will be substracted when confirming the order
+            $quantityTosubstract = $item->getQuantity();
+            $product->setQuantity($productInitialQuantity - $quantityTosubstract);
+        }
+        foreach ($user->getShoppingCartItems() as $item) {
+            $user->removeShoppingCartItem($item);
+        }
+        $em = $doctrine->getManager();
+        $em->persist($commande);
+        $em->flush();
+
+
+
+
+        return $this->redirectToRoute('home');
     }
 }
